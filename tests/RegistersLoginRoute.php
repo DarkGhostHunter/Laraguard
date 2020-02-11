6cc8066473ec9ce9285a2fd5ff17bed24cf13b40
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-02-11 14:39:14 -0300

    Added dumping error to the route testing.

--- tests/RegistersLoginRoute.php
+++ tests/RegistersLoginRoute.php
@@ -11,9 +11,14 @@ trait RegistersLoginRoute
     protected function registerLoginRoute()
     {
         Route::post('login', function (Request $request) {
-            return Auth::guard('web')->attempt($request->only('email', 'password'), $request->filled('remember'))
-                ? 'authenticated'
-                : 'unauthenticated';
+            try {
+                return Auth::guard('web')->attempt($request->only('email', 'password'), $request->filled('remember'))
+                    ? 'authenticated'
+                    : 'unauthenticated';
+            } catch (\Throwable $exception) {
+                var_dump([get_class($exception), $exception->getMessage()]);
+                throw $exception;
+            }
         })->middleware('web');
     }
 }
