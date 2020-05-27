7d7856d1985cb56435bfbb3c06cbab7c920de115
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-05-27 13:50:46 -0400

    Removed unused imports.

--- tests/Stubs/UserStub.php
+++ tests/Stubs/UserStub.php
@@ -4,8 +4,6 @@ namespace Tests\Stubs;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
-use DarkGhostHunter\Laraguard\TwoFactorAuthentication;
-use DarkGhostHunter\Laraguard\Contracts\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
 class UserStub extends Model implements AuthenticatableContract
