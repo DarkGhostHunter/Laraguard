c3815d67a8083fb8c3d03b020e3bd311bdf0a598
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-03-17 18:49:06 -0300

    Fixed minimum version for tests to pass.

--- tests/Listeners/ForcesTwoFactorAuthTest.php
+++ tests/Listeners/ForcesTwoFactorAuthTest.php
@@ -323,7 +323,7 @@ class ForcesTwoFactorAuthTest extends TestCase
             'password' => '12345678',
             'remember' => 'on',
             '2fa_code' => '',
-        ])->dump()->assertViewIs('laraguard::auth')->assertForbidden();
+        ])->assertViewIs('laraguard::auth')->assertForbidden();
     }
 
     public function test_auth_request_receives_invalid_code_shows_form()
