4530576578ce6a7830912e8122140f3cd41bb075
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-03-17 14:32:58 -0300

    Refactored Factory.

--- database/factories/TwoFactorAuthenticationFactory.php
+++ database/factories/TwoFactorAuthenticationFactory.php
@@ -1,61 +1,102 @@
 <?php
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+namespace Database\Factories;
 
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Collection;
 use DarkGhostHunter\Laraguard\Eloquent\TwoFactorAuthentication;
 
-$factory->define(TwoFactorAuthentication::class, function (Faker $faker) {
+class TwoFactorAuthenticationFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TwoFactorAuthentication::class;
 
-    $config = config('laraguard');
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $config = config('laraguard');
 
-    $array = array_merge([
-        'shared_secret' => TwoFactorAuthentication::generateRandomSecret(),
-        'enabled_at'    => $faker->dateTimeBetween('-1 year'),
-        'label'         => $faker->freeEmail,
-    ], $config['totp']);
+        $array = array_merge([
+            'shared_secret' => TwoFactorAuthentication::generateRandomSecret(),
+            'enabled_at'    => $this->faker->dateTimeBetween('-1 year'),
+            'label'         => $this->faker->freeEmail,
+        ], $config['totp']);
 
-    [$enabled, $amount, $length] = array_values($config['recovery']);
+        [$enabled, $amount, $length] = array_values($config['recovery']);
 
-    if ($enabled) {
-        $array['recovery_codes'] = TwoFactorAuthentication::generateRecoveryCodes($amount, $length);
-        $array['recovery_codes_generated_at'] = $faker->dateTimeBetween('-1 years');
+        if ($enabled) {
+            $array['recovery_codes'] = TwoFactorAuthentication::generateRecoveryCodes($amount, $length);
+            $array['recovery_codes_generated_at'] = $this->faker->dateTimeBetween('-1 years');
+        }
+
+        return $array;
     }
 
-    return $array;
-});
-
-$factory->state(TwoFactorAuthentication::class, 'with recovery', function (Faker $faker) {
-    [$enabled, $amount, $length] = array_values(config('laraguard.recovery'));
-    return [
-        'recovery_codes'              => TwoFactorAuthentication::generateRecoveryCodes($amount, $length),
-        'recovery_codes_generated_at' => $faker->dateTimeBetween('-1 years'),
-    ];
-});
-
-// This state will create a full set of safe devices, but it will leave the last as purposefully expired.
-$factory->state(TwoFactorAuthentication::class, 'with safe devices', function (Faker $faker) {
-
-    $max = config('laraguard.safe_devices.max_devices');
-
-    return [
-        'safe_devices' => Collection::times($max, function ($step) use ($faker, $max) {
-
-            $expiration_days = config('laraguard.safe_devices.expiration_days');
-
-            $added_at = $max !== $step
-                ? now()
-                : $faker->dateTimeBetween(now()->subDays($expiration_days * 2), now()->subDays($expiration_days));
+    /**
+     * Returns a model with unused recovery codes.
+     *
+     * @return TwoFactorAuthenticationFactory
+     */
+    public function withRecovery()
+    {
+        return $this->state(function(array $attributes) {
+            [$enabled, $amount, $length] = array_values(config('laraguard.recovery'));
 
             return [
-                '2fa_remember' => TwoFactorAuthentication::generateDefaultTwoFactorRemember(),
-                'ip'           => $faker->ipv4,
-                'added_at'     => $added_at,
+                'recovery_codes'              => TwoFactorAuthentication::generateRecoveryCodes($amount, $length),
+                'recovery_codes_generated_at' => $this->faker->dateTimeBetween('-1 years'),
             ];
-        }),
-    ];
-});
+        });
+    }
 
-$factory->state(TwoFactorAuthentication::class, 'disabled', [
-    'enabled_at' => null,
-]);
+    /**
+     * Returns an authentication with a list of safe devices.
+     *
+     * @return TwoFactorAuthenticationFactory
+     */
+    public function withSafeDevices()
+    {
+        return $this->state(function (array $attributes) {
+            $max = config('laraguard.safe_devices.max_devices');
+
+            return [
+                'safe_devices' => Collection::times($max, function ($step) use ($max) {
+
+                    $expiration_days = config('laraguard.safe_devices.expiration_days');
+
+                    $added_at = $max !== $step
+                        ? now()
+                        : $this->faker->dateTimeBetween(now()->subDays($expiration_days * 2), now()->subDays($expiration_days));
+
+                    return [
+                        '2fa_remember' => TwoFactorAuthentication::generateDefaultTwoFactorRemember(),
+                        'ip'           => $this->faker->ipv4,
+                        'added_at'     => $added_at,
+                    ];
+                }),
+            ];
+        });
+    }
+
+    /**
+     * Returns an enabled authentication.
+     *
+     * @return TwoFactorAuthenticationFactory
+     */
+    public function enabled()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'enabled_at' => null
+            ];
+        });
+    }
+}
